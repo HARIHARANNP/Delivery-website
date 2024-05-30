a9e25354ab27c2a67738133b2ff5de4ba9a9235a
HARIHARANNP <hariharanbe704@gmail.com>
2024-05-30 15:27:45 +0530

    Add files via upload

---
+++ scriptss.js
@@ -0,0 +1,16 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    
+    if (name && email && message) {
+        alert('Thank you for your message, ' + name + '! We will get back to you soon.');
+        
+        // Reset the form
+        this.reset();
+    } else {
+        alert('Please fill in all fields.');
+    }
+});
